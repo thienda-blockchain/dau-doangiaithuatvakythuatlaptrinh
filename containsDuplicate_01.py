7aea8164a80ddc3f3534b64ba9d4bab391843a25
thienda-blockchain <thiendaduong4444@gmail.com>
2026-02-03 08:46:58 +0700

    containsDuplicate_01.py

---
+++ containsDuplicate_01.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        so_ghi_nho = set()
+
+        for so in nums:
+            if so in so_ghi_nho:
+                return True
+
+            so_ghi_nho.add(so)
+
+        return False
+    
