6d31a051bc27ade3c2796e2cf92a2fa4821e2193
thienda-blockchain <thiendaduong4444@gmail.com>
2026-02-10 21:58:21 +0700

    Time: 20 ms (46.12%), Space: 26.1 MB (24.51%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        so_ghi_nho = set()
+
+        for so in nums:
+            if so in so_ghi_nho:
+                return True
+
+            so_ghi_nho.add(so)
+
+        return False
+
+        
