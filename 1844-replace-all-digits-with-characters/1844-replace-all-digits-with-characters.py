55070dd50980d770a741ebb83888c25df7024666
thienda-blockchain <thiendaduong4444@gmail.com>
2026-03-31 21:20:11 +0700

    Time: 0 ms (100%), Space: 12.3 MB (66.88%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
@@ -11,11 +11,8 @@ class Solution(object):
         # Đề bài cho biết các chữ số luôn nằm ở vị trí lẻ (1, 3, 5...)
         # Ta sẽ chạy vòng lặp bước nhảy 2 để tìm đúng các chữ số.
         for i in range(1, len(res), 2):
-            # Chữ cái đứng ngay trước con số hiện tại
-            char_truoc = res[i-1]
-            
-            # Giá trị của con số hiện tại (chuyển từ chữ sang số nguyên)
-            buoc_tien = int(res[i])
+            char_truoc = res[i-1]  # Chữ cái đứng ngay trước con số hiện tại
+            buoc_tien = int(res[i]) # Giá trị của con số hiện tại (chuyển từ chữ sang số nguyên)
             
             # Thực hiện phép toán "Shift" (Tịnh tiến):
             # 1. ord(char_truoc): Lấy mã số của chữ cái trước.
@@ -23,5 +20,4 @@ class Solution(object):
             # 3. chr(...): Chuyển kết quả về lại dạng chữ cái.
             res[i] = chr(ord(char_truoc) + buoc_tien)
             
-        # Nối danh sách lại thành chuỗi hoàn chỉnh để trả về
-        return "".join(res)
+        return "".join(res)   # Nối danh sách lại thành chuỗi hoàn chỉnh để trả về
