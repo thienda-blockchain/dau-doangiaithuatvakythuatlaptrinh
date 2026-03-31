2123c916ad8c8dbfa11b2f5f3d73a9a4ea316532
thienda-blockchain <thiendaduong4444@gmail.com>
2026-03-31 20:25:24 +0700

    Time: 0 ms (100%), Space: 12.3 MB (92.86%) - LeetHub

---
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def replaceDigits(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+       
+        # chuyển sang dạng danh sách (list) để dễ dàng thay thế.
+        res = list(s)
+        
+        # Đề bài cho biết các chữ số luôn nằm ở vị trí lẻ (1, 3, 5...)
+        # Ta sẽ chạy vòng lặp bước nhảy 2 để tìm đúng các chữ số.
+        for i in range(1, len(res), 2):
+            # Chữ cái đứng ngay trước con số hiện tại
+            char_truoc = res[i-1]
+            
+            # Giá trị của con số hiện tại (chuyển từ chữ sang số nguyên)
+            buoc_tien = int(res[i])
+            
+            # Thực hiện phép toán "Shift" (Tịnh tiến):
+            # 1. ord(char_truoc): Lấy mã số của chữ cái trước.
+            # 2. Cộng thêm bước tiến.
+            # 3. chr(...): Chuyển kết quả về lại dạng chữ cái.
+            res[i] = chr(ord(char_truoc) + buoc_tien)
+            
+        # Nối danh sách lại thành chuỗi hoàn chỉnh để trả về
+        return "".join(res)
